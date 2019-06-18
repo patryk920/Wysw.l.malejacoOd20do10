74c9ede1ddf82a72184cc1c39e4d339c9ba61fb1
patryk920 <pblonkowski@wp.pl>
2019-06-18 02:20:58 +0200

    Add new files

---
+++ src/Liczby.java
@@ -0,0 +1,11 @@
+/*Napisz program, który wypisze na ekranie malejąco wszystkie liczby od 20 do 10.
+
+ */
+public class Liczby {
+    public static void main(String[] args) {
+        for (int i = 20; i >= 10; i--) {
+            System.out.println(i );
+
+        }
+    }
+}
